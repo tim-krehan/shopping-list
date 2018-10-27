4d33f4416ef25e7da9ecfb6930befee6e1175c1d
Tim Krehan <mail@timkrehan.de>
2018-10-27 19:47:45 +0200

    tried some styling...

--- cont/settings.php
+++ cont/settings.php
@@ -9,8 +9,8 @@
   $user = $result->fetch_assoc();
 ?>
 <div class="userprofile">
-  <span><font class="attribute">Benutzername</font><font class="value"><input type="text" name="username" value="<?php echo $user["username"]; ?>"></font></span>
-  <span><font class="attribute">Email</font><font class="value"><input type="text" name="username" value="<?php echo $user["email"]; ?>"></font></span>
-  <span><font class="attribute">Letzter Login</font><font class="value"><?php echo $user["last_login"]; ?></font></span>
+  <span><font class="attribute">Benutzername</font><input class="change-attribute-input" type="text" name="username" value="<?php echo $user["username"]; ?>"></span>
+  <span><font class="attribute">Email</font><input class="change-attribute-input" type="text" name="username" value="<?php echo $user["email"]; ?>"></span>
+  <span><font class="attribute">Letzter Login</font><font><?php echo $user["last_login"]; ?></font></span>
 </div>
 <button class="button" id="safeButton">Speichern</button>
