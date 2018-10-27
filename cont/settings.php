97bdadfd8e82dcecc788cd120d7ee0963f08d509
Tim Krehan <mail@timkrehan.de>
2018-10-27 19:49:00 +0200

    provided code for later use

--- cont/settings.php
+++ cont/settings.php
@@ -8,9 +8,13 @@
   $result = $mysqli->query($query);
   $user = $result->fetch_assoc();
 ?>
-<div class="userprofile">
-  <span><font class="attribute">Benutzername</font><input class="change-attribute-input" type="text" name="username" value="<?php echo $user["username"]; ?>"></span>
-  <span><font class="attribute">Email</font><input class="change-attribute-input" type="text" name="username" value="<?php echo $user["email"]; ?>"></span>
-  <span><font class="attribute">Letzter Login</font><font><?php echo $user["last_login"]; ?></font></span>
+<div class="settings">
+  <div class="userprofile-pane">
+    <div class="userprofile">
+      <span><font class="attribute">Benutzername</font><input class="change-attribute-input" type="text" name="username" value="<?php echo $user["username"]; ?>"></span>
+      <span><font class="attribute">Email</font><input class="change-attribute-input" type="text" name="username" value="<?php echo $user["email"]; ?>"></span>
+      <span><font class="attribute">Letzter Login</font><font><?php echo $user["last_login"]; ?></font></span>
+    </div>
+    <button class="button" id="safeButton">Speichern</button>
+  </div>
 </div>
-<button class="button" id="safeButton">Speichern</button>
