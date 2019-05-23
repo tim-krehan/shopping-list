b5010c335aacd548293631f97dd1838199d28028
Tim Krehan <Tim.Krehan@brz.eu>
2019-05-23 09:15:50 +0200

    fixed theming

--- config/config.php
+++ config/config.php
@@ -1,10 +1,6 @@
 <?php 
 $CONFIG = array (
-  'installed' => true,
-  'host' => 'localhost',
-  'username' => 'shoppinglist',
-  'passwd' => 'Erfolg100%',
-  'database' => 'shoppinglist',
-  'theme' => 'default',
+  'installed' => false,
 );
-?>
+
+?>
