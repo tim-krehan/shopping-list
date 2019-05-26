dd1ee64c6cef40b781067e94f641f6fa0780c70e
Tim Krehan <mail@timkrehan.de>
2019-05-26 18:33:01 +0200

    added the angle right icon again

--- cont/list.php
+++ cont/list.php
@@ -39,7 +39,7 @@
         print_r("<div class='$div_item_name_classes'>$item->Name</div>");
         print_r("<div class='$div_item_menu dropdown'>");
           print_r("<button type='button' class='btn $button_theme pt-0 pb-0' id='dropdownMenuButtonDataID-" . $item->ID . "' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>");
-            print_r("...");
+            print_r("<i class='fas fa-angle-right'></i>");
           print_r("</button>");
           print_r("<div class='dropdown-menu' aria-labelledby='dropdownMenuButtonDataID-" . $item->ID . "'>");
             print_r("<button class='dropdown-item'>Action</button>");
