75f793ca4691887bb88efab3d71e7b73c8e3a4d4
Tim Krehan <mail@timkrehan.de>
2019-05-26 17:42:07 +0200

    button?

--- cont/list.php
+++ cont/list.php
@@ -33,7 +33,7 @@
             print_r("<div class='$div_item_checkbox_classes'><input type='checkbox' class='$input_item_checkbox_classes' data-color='$color_theme' data-id='$item->ID' $checked></div>");
             print_r("<div class='$div_item_quantity_classes'>$item->Anzahl $item->Einheit</div>");
             print_r("<div class='$div_item_name_classes'>$item->Name</div>");
-            print_r("<div class='$div_item_menu'><i class='fas fa-angle-right'></i></div>");
+            print_r("<button class='fas fa-angle-right $div_item_menu'></button>");
         print_r("</div>");
       }
     ?>
